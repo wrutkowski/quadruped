c01af33010721998d79208610dbcb35690a00a36
Wojciech Rutkowski <me@wojciech-rutkowski.com>
2017-04-16 15:16:02 +0100

    Ultrasonic + RFID, Ultrasonic and OLED demo

--- arduino/rfid-reader/rfid-reader.ino
+++ arduino/rfid-reader/rfid-reader.ino
@@ -14,8 +14,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         9     
-#define SS_PIN          10    
+#define RST_PIN         7     
+#define SS_PIN          6    
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
